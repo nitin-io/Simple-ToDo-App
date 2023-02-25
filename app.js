77081b27d898e1bfa2637aee70d0a8dcad152e42
Nitin Chaudhary <Nitin Chaudhary>
2023-02-25 17:40:33 +0530

    deploy ready

--- app.js
+++ app.js
@@ -130,7 +130,7 @@ app.post("/", (req, res) => {
   }
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
